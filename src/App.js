7b54b021ea40dbbf64d3ef251e7712a8926b61f2
Sam Barrick <you@example.com>
2020-01-12 15:37:07 -0600

    delete button now functional

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import './App.css';
 import FirstComponent from './FirstComponent';
 
 class App extends React.Component{
-constructor(props){
-  super(props)
-  this.state = {
+state = {
   input: '',
-  items: []
+  items: [],
+  itemId: 0
   };
-}
 
-toggle = () => {
-  this.setState({isOn: !this.state.isOn})
-}
+
+// toggle = () => {
+//   this.setState({isOn: !this.state.isOn})
+// }
+
 
 inputUpdate= event => {
   this.setState({input: event.target.value})
@@ -22,21 +22,28 @@ inputUpdate= event => {
 
 formSubmit = event => {
   event.preventDefault()
-  console.log("THIS IS STATE***", this.state.input);
+  let itemId = this.state.itemId + 1;
+  let input = this.state.input
+  let newText = {id: itemId, text: input};
+  //console.log("THIS IS STATE***", this.state.input);
   this.setState({
-    items : [...this.state.items, this.state.input],
-    input: ''
+    items: [...this.state.items, newText],
+    input: '',
+    itemId: itemId
   })
-} 
+  console.log(this.state.itemId)
+}
 
-deleteToDo = event => {
-  let array = [...this.state.items];
-  let index = array.indexOf(event.target.value);
+deleteToDo = id => {
+  let array = this.state.items;
+  let deletedTarget = array.find(x => x.id === id);
+  let index = array.indexOf(deletedTarget);
+  //debugger 
   if (index !== -1) {
-    array.splice(index, 1); 
+    array.splice(index, 1);
     this.setState({items: array});
   }
-} 
+}
 
 render(){
   console.log("see here",this.state.isOn)
@@ -50,7 +57,7 @@ render(){
   </form>
   <ul>
   {this.state.items.map((item, index) => {
-    return <li key={index}>{this.state.items[index]} <button onClick={this.deleteToDo}>Delete</button></li>
+    return <li key={index}>{item.text} <button onClick={() => this.deleteToDo(item.id)}>Delete</button></li>
   })}</ul>
   <FirstComponent items={this.state.items}/>
     </div>
@@ -58,7 +65,5 @@ render(){
 }
 
 }
-
-
 
 export default App;
